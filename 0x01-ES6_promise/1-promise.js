6aa6ad8328e9c2d9906a13344f09fa9ec98d534c
Wandzfuture <michael.zabas@gmail.com>
2024-08-07 20:29:44 +0000

    0x01. ES6 Promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,17 +1,9 @@
 /**
  * Returns a promise that resolves or rejects based on the input parameter.
  */
-function getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve({
-				status: 200,
-				body: 'Success'
-			});
-		} else {
-			reject(new Error('The fake API is not working currently'));
-		}
-	});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
+  });
 }
-
-export default getFullResponseFromAPI;
